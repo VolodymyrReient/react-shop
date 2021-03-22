12a59cd298272ab1a6cc7cf0b3ba783dbafa5eda
Volodymyr Reient <4reient4@gmail.com>
2021-03-22 10:23:27 +0200

    added link

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -4,7 +4,10 @@ function Header() {
   return (
     <nav className=" purple darken-1">
       <div className="nav-wrapper">
-        <a href="!#" className="brand-logo">
+        <a
+          href="https://volodymyrreient.github.io/react-shop/"
+          className="brand-logo"
+        >
           React Shop
         </a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
